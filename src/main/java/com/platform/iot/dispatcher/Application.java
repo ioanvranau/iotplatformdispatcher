e42037d9ac33b555d8039f167d643dd6cd119f8e
ioan.vranau <ionutvranau@gmail.com>
2016-09-02 14:48:34 +0300

    cleanup

--- src/main/java/com/platform/iot/dispatcher/Application.java
+++ src/main/java/com/platform/iot/dispatcher/Application.java
@@ -1,18 +1,12 @@
 package com.platform.iot.dispatcher;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
