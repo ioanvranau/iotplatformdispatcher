f30fae2bfd7fe6bb3b59bc7c2ecd5271440030b2
ioan.vranau <ionutvranau@gmail.com>
2016-09-02 13:02:45 +0300

    removed more

--- src/main/java/com/platform/iot/dispatcher/controller/DeviceController.java
+++ src/main/java/com/platform/iot/dispatcher/controller/DeviceController.java
@@ -6,21 +6,16 @@ package com.platform.iot.dispatcher.controller;
 
 import java.util.List;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.common.collect.Lists;
 import com.platform.iot.dispatcher.model.Device;
-import com.platform.iot.dispatcher.service.DeviceService;
 
 @RestController
 public class DeviceController {
 
     private Logger log = Logger.getLogger(DeviceController.class);
-
-    @Autowired
-    private DeviceService deviceService;
 
     @RequestMapping("/device")
     public
