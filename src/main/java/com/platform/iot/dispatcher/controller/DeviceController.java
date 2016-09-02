9f0d6f3d35ad49f67bb5b798ec7588040a8f4ebc
ioan.vranau <ionutvranau@gmail.com>
2016-09-02 14:50:44 +0300

    removed controller

--- src/main/java/com/platform/iot/dispatcher/controller/DeviceController.java
+++ src/main/java/com/platform/iot/dispatcher/controller/DeviceController.java
@@ -1,28 +1,28 @@
-package com.platform.iot.dispatcher.controller;
-
-/**
- * Created by ioan.vranau on 1/4/2016.
- */
-
-import java.util.List;
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import com.google.common.collect.Lists;
-
-@RestController
-public class DeviceController {
-
-    private Logger log = Logger.getLogger(DeviceController.class);
-
-    @RequestMapping("/device")
-    public
-    @ResponseBody
-    List<Device> getAllDevices() {
-        return Lists.newArrayList(new Device());
-    }
-
-    private class Device {
-    }
-}
+//package com.platform.iot.dispatcher.controller;
+//
+///**
+// * Created by ioan.vranau on 1/4/2016.
+// */
+//
+//import java.util.List;
+//import org.apache.log4j.Logger;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//import com.google.common.collect.Lists;
+//
+//@RestController
+//public class DeviceController {
+//
+//    private Logger log = Logger.getLogger(DeviceController.class);
+//
+//    @RequestMapping("/device")
+//    public
+//    @ResponseBody
+//    List<Device> getAllDevices() {
+//        return Lists.newArrayList(new Device());
+//    }
+//
+//    private class Device {
+//    }
+//}
